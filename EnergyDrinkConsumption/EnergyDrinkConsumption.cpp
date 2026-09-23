cea3233b358be811bba3d87ef2f2356fdd07cf51
Javier Toscano <jst2476@email.vccs.edu>
2026-09-23 19:33:27 -0400

    edited to show whole numbers and spacing.

--- EnergyDrinkConsumption/EnergyDrinkConsumption.cpp
+++ EnergyDrinkConsumption/EnergyDrinkConsumption.cpp
@@ -11,12 +11,12 @@ int main()
 	const double percentageEnergy = 0.15;
 	const double percentageCitrus = 0.58;
 	double energy_drink_customers = totalCustomers * percentageEnergy;
-	double citrus_customers = energy_drink_customers * percentageCitrus;
+	int citrus_customers = energy_drink_customers * percentageCitrus;
 
 	// Diplays.
 
-	cout << "Customers who purchase energy drinks weekly: " << energy_drink_customers;
-	cout << "Customers who prefer citrus energy drinks: " << citrus_customers;
+	cout << "Customers who purchase energy drinks weekly: " << energy_drink_customers << endl;
+	cout << "Customers who prefer citrus flavored energy drinks: " << citrus_customers;
 	return 0;
 }
 
