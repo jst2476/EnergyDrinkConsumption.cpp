d6f4cf81a9794a0433cf580abc9c31be35c0179f
Javier Toscano <jst2476@email.vccs.edu>
2026-09-23 19:06:00 -0400

    entered cout statements and made some edits

--- EnergyDrinkConsumption/EnergyDrinkConsumption.cpp
+++ EnergyDrinkConsumption/EnergyDrinkConsumption.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 int main()
 {
-	cout << "Results From Surveyed Customers" << endl;
+	cout << "Results From 16,500 Surveyed Customers!" << endl;
+
 	//Constants, Variables, and Calculations.
+
 	const int totalCustomers = 16500;
 	const double percentageEnergy = 0.15;
 	const double percentageCitrus = 0.58;
 	double energy_drink_customers = totalCustomers * percentageEnergy;
 	double citrus_customers = energy_drink_customers * percentageCitrus;
+
+	// Diplays.
+
+	cout << "Customers who purchase energy drinks weekly: " << energy_drink_customers;
+	cout << "Customers who prefer citrus energy drinks: " << citrus_customers;
 
 }
 
